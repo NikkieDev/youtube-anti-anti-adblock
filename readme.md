a609d35d73cb2bcd74522a36271b7a719035746a
NikkieDev <dev.nikkie@gmail.com>
2023-11-09 00:01:35 +0100

    updated: added several options & an improved UI

--- readme.md
+++ readme.md
@@ -1,4 +1,3 @@
 # Chrome Extension
 ### All it does is block the "Adblocker detected" popup on youtube, and keep track of how may have been blocked
 ### by clicking on it, you can see how many have already been blocked.
-
--- src/blocker.js
+++ src/blocker.js
@@ -1,7 +1,7 @@
 const __data = { popupsRemoved: 0 };
 
-function setup() {
-	chrome.storage.sync.get(["popupsRemoved"], (result) => {
+async function setup() {
+	await chrome.storage.sync.get(["popupsRemoved"], (result) => {
 		if (result.popupsRemoved !== undefined) {
 			__data.popupsRemoved = result.popupsRemoved;
 		} else chrome.storage.sync.set({ popupsRemoved: __data.popupsRemoved });
@@ -12,24 +12,30 @@ function setPlay(elem) {
 	elem.click();
 }
 
-function checkForPopup() {
+async function checkForPopup() {
 	const myDrillah = document.querySelector("tp-yt-iron-overlay-backdrop.opened");
 	const myDrillahTwo = document.querySelector("ytd-enforcement-message-view-model");
 	const myDrillahThree = document.querySelector("button.ytp-play-button");
 
 	if (myDrillah != null && myDrillahTwo != null) {
-		myDrillah.classList.remove("opened");
-		myDrillahTwo.parentElement.remove();
-		setPlay(myDrillahThree);
+		await chrome.storage.sync.get(["popupsPauseOption", "popupsTrackOption"], function(result) {
+			if (result.popupsPauseOption == false) {
+				myDrillah.classList.remove("opened");
+				myDrillahTwo.parentElement.remove();
+				setPlay(myDrillahThree);
 
-		__data.popupsRemoved++;
-		chrome.storage.sync.set({ popupsRemoved: __data.popupsRemoved });
-		
-		console.info("Removed popup!");
+				if (result.popupsTrackOption == true) {
+					__data.popupsRemoved++;
+					chrome.storage.sync.set({ popupsRemoved: __data.popupsRemoved });
+				}
+				
+				console.info("Removed popup!");
+			}
+		})
 	}
-}(function () {
+}(async function () {
 	"use strict";
-	setup();
-	checkForPopup();
-	setInterval(checkForPopup, 2000);
+	await setup();
+	await checkForPopup();
+	setInterval(await checkForPopup, 2000);
 })();
